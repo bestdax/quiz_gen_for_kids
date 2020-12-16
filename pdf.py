37989f41b0d1e268500d196dfc07a185cf4fe376
dax <dax26d@gmail.com>
2020-12-16 15:16:00 +0800

    因为PDF库改了，ln参数不需要了。

--- pdf.py
+++ pdf.py
@@ -17,18 +17,18 @@ class PDF(FPDF):
     def set_title(self, title):
         self.set_xy(0, 0)
         self.set_font("Yahei", '', 24)
-        self.cell(210, 40, txt=title, border=0, ln=2, align="C")
+        self.cell(210, 40, txt=title, border=0, align="C")
 
     def set_date(self, date=True):
         self.set_xy(0, 20)
         self.set_font("Yahei", '', 14)
         underline = '_' * 13
         if date:
-            self.multi_cell(195, 10, txt=today_string(), border=0, ln=2, align="R")
+            self.multi_cell(195, 10, txt=today_string(), border=0, align="R")
         else:
-            self.multi_cell(195, 10, txt='', border=0, ln=2, align='R')
+            self.multi_cell(195, 10, txt='', border=0, align='R')
         self.multi_cell(210, 13, txt='姓名' + underline + '开始时间' + underline + '结束时间' + underline + '得分' + underline,
-                        border=0, ln=2, align="C")
+                        border=0, align="C")
 
     def set_quizzes(self, quizzes):
         start_x, start_y = 15, 50
@@ -45,7 +45,7 @@ class PDF(FPDF):
             formula = quizzes[index]
             next_x = start_x + cell_width * (col + 1) if col < 2 else start_x
             next_y = start_y + cell_height * row if col < 2 else start_y + cell_height * (row + 1)
-            self.multi_cell(cell_width, cell_height, txt=formula, border=0, ln=2, align="L")
+            self.multi_cell(cell_width, cell_height, txt=formula, border=0, align="L")
             self.set_xy(next_x, next_y)
 
 

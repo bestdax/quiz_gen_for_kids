74c41ae92fda5d5e05437c7e0299749aa04b5440
dax <dax26d@gmail.com>
2020-12-27 08:15:56 +0800

    modify translate.txt to resource path

--- translate.py
+++ translate.py
@@ -1,12 +1,5 @@
-import os
-import sys
 from collections import defaultdict
-
-
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS2', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
+from utils import resource_path
 
 
 def translate(string):
